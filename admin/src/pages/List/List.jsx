1ce0152b1453458d28878f63ea749bffa5eb0be8
Ali <hoccalo@gmail.com>
2024-05-05 06:45:19 +0300

    add and list foods

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 const List = () => {
-  return <div>List</div>;
+  const url = import.meta.env.VITE_BASE_ENDPOINT;
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const res = await axios.get(`${url}/api/food/list`);
+    console.log(res.data);
+    if (res.data.success) {
+      setList(res.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div className="list add flex-col">
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/` + item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p>X</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default List;
