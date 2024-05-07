73cfef9ff630c2f685e09c1ac40b60f82353df06
Ali <hoccalo@gmail.com>
2024-05-07 07:44:57 +0300

    cart endpoints

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,7 +10,6 @@ const userSchema = new mongoose.Schema(
   },
   { minimize: false }
 );
-
 const userModel = mongoose.model.user || mongoose.model("user", userSchema);
 
 export default userModel;
